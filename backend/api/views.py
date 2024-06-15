fa251cfe403eb2984ccd8fe841ba99dc3221fe27
Evan <evan.ucd@gmail.com>
2024-06-15 23:18:41 +0100

    serialization for login and register

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from api import serializer as api_serializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from userauths.models import User, Profile
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
