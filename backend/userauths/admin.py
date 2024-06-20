2c6883dd826da5ab5f22ba7b304156a6599a0641
Evan <evan.ucd@gmail.com>
2024-06-20 17:03:12 +0100

    all files updata

--- backend/userauths/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Profile
-
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'full_name', 'date']
-
-admin.site.register(User)
-admin.site.register(Profile, ProfileAdmin)
-
