37a61e0a048c924ca38eec4fb0ad587a99225315
Evan <evan.ucd@gmail.com>
2024-06-23 20:25:52 +0100

    for token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     # path("user/change-password/", api_views.ChangePasswordAPIView.as_view()),
     #
     # # Core Endpoints
-    # path("course/category/", api_views.CategoryListAPIView.as_view()),
+    path("course/category/", api_views.CategoryListAPIView.as_view()),
     # path("course/course-list/", api_views.CourseListAPIView.as_view()),
     # path("course/search/", api_views.SearchCourseAPIView.as_view()),
     # path("course/course-detail/<slug>/", api_views.CourseDetailAPIView.as_view()),
